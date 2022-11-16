511203d566debbe407ecd0d968a8e9e08510b4f9
RokibulObin <rokibulo@bzz.ch>
2022-11-16 10:42:55 +0100

    checkout GUI Branch

---
+++ app/src/main/java/com/example/securityagent/PWAendern.java
@@ -0,0 +1,14 @@
+package com.example.securityagent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PWAendern extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pwaendern);
+    }
+}
